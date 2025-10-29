d8a86bed7fd5132bc4f2f9cb00af9e8926652a88
Ali Yuce <yucee76@gmail.com>
2025-10-29 19:20:52 +0100

    Initial commit

---
+++ app/src/main/java/com/aliyuce/rickandmorty/data/remote/Models.kt
@@ -0,0 +1,45 @@
+package com.aliyuce.rickandmorty.data.remote
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Info(
+    val count: Int,
+    val pages: Int,
+    val next: String? = null,
+    val prev: String? = null,
+)
+
+@Serializable
+data class EpisodeResponse(
+    val info: Info,
+    val results: List<Episode>
+)
+
+@Serializable
+data class Episode(
+    val id: Int,
+    val name: String,
+    @SerialName("air_date") val airDate: String,
+    val episode: String,
+    val characters: List<String>
+)
+
+@Serializable
+data class Character(
+    val id: Int,
+    val name: String,
+    val status: String? = null,
+    val species: String? = null,
+    val image: String? = null,
+    val origin: Origin? = null,
+    val episode: List<String> = emptyList()
+)
+
+@Serializable
+data class Origin(
+    val name: String,
+    val url: String? = null
+)
+
---
+++ app/src/main/java/com/aliyuce/rickandmorty/data/remote/RickAndMortyApi.kt
@@ -0,0 +1,17 @@
+package com.aliyuce.rickandmorty.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface RickAndMortyApi {
+    @GET("/api/episode")
+    suspend fun getEpisodes(@Query("page") page: Int = 1): EpisodeResponse
+
+    @GET("/api/episode/{id}")
+    suspend fun getEpisode(@Path("id") id: Int): Episode
+
+    @GET("/api/character/{id}")
+    suspend fun getCharacter(@Path("id") id: Int): Character
+}
+
